27f338f44ec8d872c9d815fae94d43c44687c41a
Russell Jowell <russ.jowell@gmail.com>
2021-10-11 11:08:07 -0400

    Initial check-in of module L7_Test_App

---
+++ L7_Test_App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace L7_Test_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
